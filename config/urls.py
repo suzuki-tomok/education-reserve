ef459605468673fee3c7f606e9ca27aa63b8ebdd
suzuki-tomok <suzuki.tomok@outlook.jp>
2026-02-07 15:59:34 +0900

    fix: Admin管理サイトのタイトル変更

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+admin.site.site_header = "Education Reserve 管理サイト"
+admin.site.site_title = "Education Reserve"
+admin.site.index_title = "管理メニュー"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.core.api.urls")),
